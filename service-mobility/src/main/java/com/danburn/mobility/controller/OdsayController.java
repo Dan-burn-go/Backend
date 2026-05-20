f8120ce2c990581638527235d4fb4968ffca9e56
권혁태 <axnightax@naver.com>
2026-05-20 21:30:32 +0900

    feat: ODsayService 및 ODsayToClient 매퍼 클래스 구현(#302)

--- service-mobility/src/main/java/com/danburn/mobility/controller/OdsayController.java
+++ service-mobility/src/main/java/com/danburn/mobility/controller/OdsayController.java
@@ -1,4 +1,30 @@
 package com.danburn.mobility.controller;
 
+import com.danburn.common.response.ApiResponse;
+import com.danburn.mobility.dto.request.OdsayApiRequest;
+import com.danburn.mobility.dto.response.TransitRouteResponse;
+import com.danburn.mobility.service.OdsayService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/mobility")
 public class OdsayController {
+
+    private final OdsayService odsayService;
+
+    @GetMapping("/route")
+    public ApiResponse<TransitRouteResponse> getRoute(
+            @RequestParam Double startLng,
+            @RequestParam Double startLat,
+            @RequestParam Double endLng,
+            @RequestParam Double endLat
+    ) {
+        OdsayApiRequest odsayApiRequest = new OdsayApiRequest(startLng, startLat, endLng, endLat);
+        return ApiResponse.ok(odsayService.fetchOdsayRoute(odsayApiRequest));
+    }
 }
